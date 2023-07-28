bf27e50a703780d9d25fdb1d2cfb90f7ce585d06
Wangs_Official <wangsofficial@outlook.com>
2023-07-28 13:54:24 +0800

    [feat]V3完成✅V4准备

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 <img src = 'https://s3.bmp.ovh/imgs/2023/07/21/a43006f865b8eaf7.png' >
 <h2>欢迎使用SovitsBox</h2>
 <h3>SovitsBox服务端文档（SovitsBoxAPI）</h3>
+<img src = 'https://img.shields.io/badge/%E5%BC%80%E6%BA%90%E8%AE%B8%E5%8F%AF%E8%AF%81-MIT-blue'>
+  <img alt="GitHub contributors" src="https://img.shields.io/github/contributors/wangs-official/sovitsbox-api">
+
 </div>
+
 
 
 
@@ -49,178 +53,341 @@ SovitsBox分为服务端（SovitsBoxAPI）以及客户端（SovitsBoxAPP）。�
 
 ## API文档
 
-除推理使用POST请求外，均使用GET方式请求，请求地址：
+请求地址：3.0为 `127.0.0.1:65503` 4.0为 `127.0.0.1:65504`
 
-- Sovits3.0服务端 127.0.0.1:65503
+1. 状态 (3.0/4.0)
 
-- Sovits4.0服务端 127.0.0.1:65504
+   简要描述
 
-  状态码：`正常 200` `异常 800 ` `不是有效的参数 801`
+   - 测试接口是否正常
 
-  
+   请求地址
 
-1. 状态
+   - `/`
 
-用于检测服务端是否启动
+   请求方式
 
-请求地址 `/`
+   - GET
 
-参数：无
+   参数
 
-正常返回值：
+   无
 
-|  KEY   | VALUE |  TYPE  | 兼容性  |
-| :----: | :---: | :----: | :-----: |
-| status | 状态  | string | 3.0/4.0 |
+   返回示例
 
+   ```json
+   {
+     'status':'200OK'
+   }
+   ```
 
+   返回参数说明
 
-2. 下载hubert模型文件
+   | 参数名 |  类型  |      说明       |
+   | :----: | :----: | :-------------: |
+   | status | string | 返回200OK则正常 |
 
-用于下载Hubert模型文件
 
-请求地址`/download_hubert`
 
-参数：
+2. 下载Hubert模型文件 (3.0/4.0)
 
-|   KEY    | VALUE | TYPE | 兼容性  | 是否必选 |
-| :------: | :---: | :--: | :-----: | :------: |
-| download | true  | bool | 3.0/4.0 |    是    |
+简要描述
 
-例子：`127.0.0.1:65503/download_hubert?download=true`
+- 下载Hubert模型文件
 
-返回值
+请求地址
 
-正常返回值：
+- `/download_hubert`
 
-|       KEY       | VALUE |  TYPE  | 兼容性  |
-| :-------------: | :---: | :----: | :-----: |
-| download_status |  OK   | string | 3.0/4.0 |
+请求方式
 
-异常返回值：
+- GET
 
-|     KEY     |        VALUE         |  TYPE  | 兼容性  |
-| :---------: | :------------------: | :----: | :-----: |
-| PythonError | `Python执行错误返回` | string | 3.0/4.0 |
+参数
 
+|  参数名  | 必选 | 类型 |          说明          |
+| :------: | :--: | :--: | :--------------------: |
+| download |  是  | bool | 是否下载，若下载填true |
 
+正常返回示例
 
-3. 加载模型
+```json
+{
+  'download_status': 'OK'
+}
+```
 
-请先加载模型再进行推理！
+正常返回参数说明
 
-请求地址 `/loadmodel`
+|     参数名      |  类型  |     说明     |
+| :-------------: | :----: | :----------: |
+| download_statud | string | 返回OK则正常 |
 
-参数：
+异常返回（800）示例
 
-|    KEY     |       VALUE       |  TYPE  | 兼容性  | 是否必选 |
-| :--------: | :---------------: | :----: | :-----: | :------: |
-| model_path |    `模型路径`     | string | 3.0/4.0 |    是    |
-| json_path  | `config.json路径` | string | 3.0/4.0 |    是    |
+```json
+{
+  'PythonError': 'xxx'
+}
+```
 
-例子：`127.0.0.1:65503/loadmodel?model_path=/example/example.pth&json_path=/example/config.json`
+异常返回参数说明
 
-返回值
+|   参数名    |  类型  |         说明         |
+| :---------: | :----: | :------------------: |
+| PythonError | string | Python执行时发生错误 |
 
-正常返回值：
+3. 删除raw/results文件夹内的所有文件 (3.0/4.0)
 
-|    KEY     |    VALUE     |  TYPE  | 兼容性  |
-| :--------: | :----------: | :----: | :-----: |
-|   status   |      OK      | string | 3.0/4.0 |
-| speaker_ls | `说话人列表` | string | 3.0/4.0 |
+简要描述
 
-异常返回值：
+- 我怕小白连文件都不会删
 
-|         KEY         |                            VALUE                             |  TYPE  | 兼容性  |
-| :-----------------: | :----------------------------------------------------------: | :----: | :-----: |
-|     PythonError     |                     `Python执行错误返回`                     | string | 3.0/4.0 |
-| HubertNotFoundError | hubert模型不存在，请访问URL：http://127.0.0.1:65503/download_hubert?download=true 下载 | string |   3.0   |
-| HubertNotFoundError | hubert模型不存在，请访问URL：http://127.0.0.1:65504/download_hubert?download=true 下载 | string |   4.0   |
-|      FileError      |                        `文件错误返回`                        | string | 3.0/4.0 |
-|        Error        |                        `通用错误返回`                        | String | 3.0/4.0 |
+请求地址
 
-4. 删除raw/results文件夹内的所有文件
+- `/delete_file`
 
-我感觉小白不会删文件所以就写了这个接口
+请求方式
 
-请求地址 `/delete_file`
+- GET
 
-参数：
+参数
 
-| KEY  |    VALUE    |  TYPE  | 兼容性  | 是否必选 |
-| :--: | :---------: | :----: | :-----: | :------: |
-| del  | raw/results | string | 3.0/4.0 |    是    |
+| 参数名 | 必选 |  类型  |        说明         |
+| :----: | :--: | :----: | :-----------------: |
+|  del   |  是  | string | raw/results任选其一 |
 
-例子`127.0.0.1:65503/delete_file?del=raw`
+正常返回示例
 
-返回值
+```json
+{
+  'status': 'OK'
+  'del_path': 'raw'
+}
+```
 
-正常返回值：
+正常返回参数说明
 
-|   KEY    |    VALUE    |  TYPE  | 兼容性  |
-| :------: | :---------: | :----: | :-----: |
-|  status  |     OK      | string | 3.0/4.0 |
-| del_path | raw/results | string | 3.0/4.0 |
+|  参数名  |  类型  |       说明       |
+| :------: | :----: | :--------------: |
+|  status  | string |   返回OK则正常   |
+| del_path | string | 删除的是哪个文件 |
 
-异常返回值：
+异常返回（800）示例
 
-|     KEY     |        VALUE         |  TYPE  | 兼容性  |
-| :---------: | :------------------: | :----: | :-----: |
-| PythonError | `Python执行错误返回` | string | 3.0/4.0 |
-|    Error    |    不是有效的参数    | string | 3.0/4.0 |
+```json
+{
+  'PythonError': 'xxx'
+}
+```
 
-5.  上传wav文件到raw目录
+异常返回参数说明
 
-没用
+|   参数名    |  类型  |         说明         |
+| :---------: | :----: | :------------------: |
+| PythonError | string | Python执行时发生错误 |
 
-请求地址 `/upload_wav`
+异常返回（801）：参数填写错误
 
-参数：
+4. 上传wav文件到raw文件夹 (3.0/4.0)
 
-|   KEY    |     VALUE     |  TYPE  | 兼容性  | 是否必选 |
-| :------: | :-----------: | :----: | :-----: | :------: |
-| wav_path | `wav文件路径` | string | 3.0/4.0 |    是    |
+简要描述
 
-例子`127.0.0.1:65503/upload_wav?wav_path=example/aaa.wav`
+- 我怕小白不会上传文件
 
-返回值
+请求地址
 
-正常返回值：
+- `/upload_wav`
 
-|       KEY        |        VALUE        |  TYPE  | 兼容性  |
-| :--------------: | :-----------------: | :----: | :-----: |
-|      status      |         OK          | string | 3.0/4.0 |
-| upload_file_path | `上传的wav文件路径` | string | 3.0/4.0 |
+请求方式
 
-异常返回值：
+- GET
 
-|     KEY     |          VALUE           |  TYPE  | 兼容性  |
-| :---------: | :----------------------: | :----: | :-----: |
-| PythonError |   `Python执行错误返回`   | string | 3.0/4.0 |
-|    Error    |      不是有效的参数      | string | 3.0/4.0 |
-|  FileError  | 目标文件不存在/非wav文件 | String | 3.0/4.0 |
+参数
 
-6. 推理
+|  参数名  | 必选 |  类型  |        说明         |
+| :------: | :--: | :----: | :-----------------: |
+| wav_path |  是  | string | 待上传的Wav文件路径 |
 
-推理前请先加载模型！（仅限3.0需要提前加载模型）使用POST请求
+返回示例
 
-请求地址 `/inference`
+```json
+{
+  'status':'OK'
+  'upload_file_path': '/example/eg.wav'
+}
+```
 
-**由于3.0和4.0的推理方式完全不相同，接下来将分为两个板块来写文档**
+返回参数说明
 
-#### 3.0
+|      参数名      |  类型  |      说明       |
+| :--------------: | :----: | :-------------: |
+|      status      | string | 返回200OK则正常 |
+| upload_file_path | string | 上传wav文件路径 |
 
-参数：
+异常返回（800）示例
 
-|      KEY       |                            VALUE                             |  TYPE  | 是否必选 |
-| :------------: | :----------------------------------------------------------: | :----: | :------: |
-| clean_names_ls | `请填入准备推理的wav文件名（无需输入.wav）多文件使用空格转义符号隔开'\'` | string |    是    |
-|   trans_num    | `音高调整，支持正（半）音，填入调整半音数量，无需调整请填入0` |  int   |    是    |
-|  spk_list_ls   | `支持多说话人合成，请填入说话人，多说话人使用空格转义符号隔开'\'` | string |    是    |
-|    slice_db    | `填入slice_db，嘈杂的音频可以-30，干声保留呼吸可以-50 , 无需调整请填入-40` |  int   |    是    |
-|   file_name    | `文件名，有以下参数:{clean_name} 干声文件名 {tran} 音高调整数 {spk} 说话人` | string |    否    |
-|  output_file   |                   `输出文件格式，默认wav`                    | string |    否    |
+```json
+{
+  'PythonError': 'xxx'
+}
+```
 
+异常返回参数说明
 
+|   参数名    |  类型  |           说明           |
+| :---------: | :----: | :----------------------: |
+| PythonError | string |   Python执行时发生错误   |
+|  FileError  | string | 目标文件不存在/非wav文件 |
 
-## 开发中...
+异常返回（801）：参数填写错误
+
+5. 推理 (3.0)
+
+简要描述
+
+- 没有
+
+请求地址
+
+- `/inference`
+
+请求方式
+
+- GET
+
+参数
+
+|      参数名      | 必选 |  类型  |                             说明                             |
+| :--------------: | :--: | :----: | :----------------------------------------------------------: |
+|  clean_names_ls  |  是  | string | 干声路径，支持多个wav文件,请将其放入raw文件夹内，多文件使用半角逗号隔开，无需输入文件扩展名 |
+|    trans_num     |  是  |  int   |           音高调整，支持正（半音），无需调整填入 0           |
+|   spk_list_ls    |  是  | string |                支持多说话人，使用半角逗号隔开                |
+|   slice_db_num   |  是  |  int   |  嘈杂的音频可以-30，干声保留呼吸可以-50 , 无需填入请输入-40  |
+| output_file_type |  否  | string |                           默认wav                            |
+|    model_path    |  是  | string |                           模型路径                           |
+|    json_path     |  是  | string |                       Config.json路径                        |
+
+返回示例
+
+```json
+{
+  'status': 'OK', 
+  'output_wav_path': '/results/eg.wav'
+}
+```
+
+返回参数说明
+
+|     参数名      |  类型  |       说明       |
+| :-------------: | :----: | :--------------: |
+|     status      | string | 返回200OK则正常  |
+| output_wav_path | string | 推理后的文件路径 |
+
+异常返回（800）示例
+
+```json
+{
+  'PythonError': 'xxx'
+}
+```
+
+异常返回参数说明
+
+|    参数名     |  类型  |           说明            |
+| :-----------: | :----: | :-----------------------: |
+|  PythonError  | string |   Python执行时发生错误    |
+| PthFileError  | string | 目标文件不存在/非pth文件  |
+| JsonFileError | string | 目标文件不存在/非json文件 |
+
+异常返回（801）：参数填写错误
+
+## 使用前提示/用户协议
+
+### 使用前提示
+
+SovitsBoxAPI（SovitsBox服务端）仅面向于高级用户使用，若您是小白，请使用SovitsBox。若已知自己没有使用API基础仍要使用，请在遇到问题时自己百度/Google/Bing，我们不予解决任何非程序问题。
+
+请善用Issues功能，在发起Issues前请务必确认该问题是一个有价值的问题，否则不予解答
+
+关于贡献问题，请看下方
+
+### 用户协议
+
+本用户协议（以下简称“协议”）是由SovitsBoxAPI/SovitsBoxAPP制作者（以下简称“我们”或“项目”）与使用本项目的用户（以下简称“您”或“用户”）之间订立的。本协议规定了您使用本项目的条款和条件。请您仔细阅读本协议，并在使用本项目之前同意遵守本协议。如果您不同意本协议，您将无法使用本项目。
+
+1. 项目介绍
+
+   本项目使用Python语言开发，基于SO-VITS-SVC项目。项目旨在为用户提供一种改变声音特征的工具，可以用于娱乐、教育、研究等目的。项目使用MIT许可证发布，任何人都可以在遵守许可证条款的前提下使用、复制、修改和分发项目。
+
+2. 用户权利和义务
+
+   - 您有权在遵守本协议和MIT许可证条款的前提下，免费使用、复制、修改和分发本项目。
+   - 您有义务保护自己和他人的隐私和知识产权，不得使用本项目收集、存储、传播或泄露他人的个人信息或版权内容，除非您已获得他人的明确授权。
+   - 您有义务遵守所有适用的法律法规，不得使用本项目进行非法、不道德或有害活动。这些活动包括但不限于欺诈、诽谤、骚扰、威胁、侵犯他人隐私或知识产权等。
+   - 您有义务对自己使用本项目的方式和目的负全责，并承担由此产生的一切风险和后果。
+
+3. 项目责任免除
+
+   - 本项目是按照“现状”提供的，我们不对本项目的正确性、完整性、可靠性、安全性或适用性做出任何明示或暗示的保证或声明。
+   - 我们不对任何因使用或无法使用本项目而造成的任何直接或间接的损失、损害、索赔或责任负责。
+   - 我们不支持、鼓励或容忍任何使用本项目进行非法、不道德或有害活动的行为。如果我们发现或被告知您违反了本协议或任何适用的法律法规，我们担任任何法律责任
+
+4. 协议修改和更新
+
+   我们有权根据我们的判断随时修改或更新本协议。我们会在我们认为合适的方式通知您任何重大变更或更新。您应定期查看本协议，以了解任何变更或更新。您继续使用本项目即表示接受本协议及其任何修订。
+
+## 贡献
+
+首先，感谢您对本项目的重视
+
+由于开发时未使用统一的规定，导致了代码可读性下降等恶性问题
+
+您可以对此项目做出优化，请继续看下方
+
+### 文件树（展示的是对你有用的）
+
+```
+SovitsBox-API
+├─ update.py（更新用程序）
+├─ requirements.txt（需求库安装）
+├─ README.md（README）
+├─ .gitignore（基本已经搞完了）
+├─ v3（V3）
+│  ├─ app.py（V3的app.py）
+├─ v3（V4）
+│  ├─ app.py（V4的app.py）
+└─ update（更新目录）
+	 ├─ tmp（从Github下载的ver.json放在这里，这个文件夹不提交）
+	 │  ├─ ver.json
+   └─ ver.json（本地ver.json，请在每次提交前将该文件内的版本号+1 如原来0.1.11 提交前改为0.1.12）
+```
+
+### 提交标签
+
+`[feat]`添加新功能
+
+`[fix]`修复BUG
+
+`[opt]`优化代码
+
+`[dev->main]`合并dev分支到main分支
+
+`[upd]`版本号文件更新
+
+### 其他
+
+请创建PR到 dev 分支而不是 main 分支
+
+dev分支定时合并到main分支
+
+## 后言
+
+感谢您使用SovitsBox！
+
+### 感谢以下贡献者
+
+<a href="https://github.com/wangs-official/sovitsbox-api/contributors">
+  <img src="https://contrib.rocks/image?repo=wangs-official/sovitsbox-api" />
+</a>
+
--- v3/app.py
+++ v3/app.py
@@ -29,13 +29,14 @@
 # 先提示一下
 
 import time
+
 print("SovitsBox服务端(版本3.0)\n请提前安装好所有库(具体请查看README)\n将在3s后启动")
 time.sleep(3)
 
 # 引用必要库
 
 print("正在加载库")
-from flask import Flask, jsonify , request
+from flask import Flask, jsonify, request
 import werkzeug
 import shutil
 import json
@@ -54,30 +55,33 @@ from inference import infer_tool
 from inference import slicer
 from inference.infer_tool import Svc
 
+
 # 自定义HTTP错误子类
 class InsufficientStorage(werkzeug.exceptions.HTTPException):
-    code=800
+    code = 800
     description = 'exception return'
 
+
+# 变量提前定义
+
+model_load_status = str(0)  # 模型加载状态，参考下方加载模型接口
+
+
 class InsufficientStorage(werkzeug.exceptions.HTTPException):
-    code=801
+    code = 801
     description = 'parametric error'
+
 
 print('库加载完成，正在启动...')
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-# 状态
-# 用法：GET请求
-# 无请求参数
+
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({'status': '200OK'})
 
-# 下载hubert模型
-# 用法：GET请求
-# 请求参数
-# KEY:download VALUE:是否下载 TYPE:boolean 必须
+
 @app.route('/download_hubert', methods=['GET'])
 def download_hubert():
     # 获取参数
@@ -93,117 +97,157 @@ def download_hubert():
         except Exception as e:
             # 如果发生错误，返回异常信息
             print("服务端:Python发生错误：" + str(e))
-            return jsonify({'PythonError': str(e)}) , 800
+            return jsonify({'PythonError': str(e)}), 800
 
 
-# 加载模型接口
-# 用法：GET请求
-# 请求参数
-# KEY:model_path VALUE:模型路径 TYPE:string 必须
-# KEY:json_path VALUE:config.json路径 TYPE:string 必须
-@app.route('/loadmodel', methods=['GET'])
-def loadmodel():
-    model_path = request.args.get('model_path')
-    json_path = request.args.get('json_path')
-    try:
-        if not model_path is None:
-            if not json_path is None:
-                if os.path.isfile(model_path) and model_path.endswith('pth'):  # 验证文件路径以及后缀名
-                    print("服务端:已导入模型文件 路径:" + model_path)
-                else:
-                    return jsonify({'FileError' : '目标文件不存在/非pth文件'}) , 800
-
-                if os.path.isfile(json_path) and json_path.endswith('json'): # 验证文件路径以及后缀名
-                    print('服务端:已导入Config文件' + json_path)
-                else:
-                    return jsonify({'FileError' : '目标文件不存在/非json文件'}) , 800
-            else:
-                return jsonify({'Error': '不是有效的参数'}) , 801
-        else:
-            return jsonify({'Error': '不是有效的参数'}) , 801
-
-        # 加载模型与创建文件夹
-        svc_model = Svc(model_path, json_path)
-        infer_tool.mkdir(["raw", "results"])
-        model_load_status = str(1) # 模型加载后写入变量
-
-        #以下代码为读取说话人列表
-        with open(json_path, 'r') as config_json:
-            config_json_speaker = json.load(config_json)
-
-        for config_json_speaker_ls in config_json_speaker['spk']:
-            print('服务端:从Config.json文件中找到了以下说话人：' + config_json_speaker_ls)
-
-        print('服务端:模型加载已完成')
-        return jsonify({'status':'OK','speaker_ls':config_json_speaker_ls})
-
-
-    except FileNotFoundError:
-        # 如果Hubert文件不存在，那就特殊报错（？）
-        print("服务端:hubert模型不存在，请访问URL：http://127.0.0.1:65503/download_hubert?download=true 下载")
-        return jsonify({'HubertNotFoundError': 'hubert模型不存在，请访问URL：http://127.0.0.1:65503/download_hubert?download=true 下载'}) , 800
-
-    except Exception as e:
-        # 如果发生错误，返回异常信息
-        print("服务端:Python发生错误：" + str(e))
-        return jsonify({'PythonError': str(e)}) , 800
-
-# 删除raw/results文件夹内的所有文件
-# 用法：GET请求
-# 请求参数
-# KEY:del VALUE:raw/results TYPE:string 必须
 @app.route('/delete_file', methods=['GET'])
 def delete_file():
     try:
         delete_file_path = request.args.get('del')
         if delete_file_path == str('raw'):
-            #删除raw文件夹
-            shutil.rmtree('raw' , ignore_errors=True)
+            # 删除raw文件夹
+            shutil.rmtree('raw', ignore_errors=True)
             infer_tool.mkdir(["raw"])
             print('服务端:文件夹 raw 内的文件已被删除')
-            return jsonify({'status' : 'OK' , 'del_path' : 'raw'})
+            return jsonify({'status': 'OK', 'del_path': 'raw'})
         elif delete_file_path == str('results'):
-            #删除results文件夹
-            shutil.rmtree('results' , ignore_errors=True)
+            # 删除results文件夹
+            shutil.rmtree('results', ignore_errors=True)
             infer_tool.mkdir(["results"])
             print('服务端:文件夹 results 内的文件已被删除')
-            return jsonify({'status' : 'OK' , 'del_path' : 'results'})
+            return jsonify({'status': 'OK', 'del_path': 'results'})
         else:
-            return jsonify({'Error': '不是有效的参数'}) , 801
+            return jsonify({'Error': '不是有效的参数'}), 801
 
     except Exception as e:
         # 如果发生错误，返回异常信息
         print("服务端:Python发生错误：" + str(e))
-        return jsonify({'PythonError': str(e)})  , 800
+        return jsonify({'PythonError': str(e)}), 800
 
-# 上传wav文件到raw目录
-# 用法：GET请求
-# 请求参数
-# KEY:wav_path VALUE:wav文件路径 TYPE:string 必须
-@app.route('/upload_wav' , methods=['get'])
+
+@app.route('/upload_wav', methods=['get'])
 def upload_wav():
     try:
         wav_path_upload = request.args.get('wav_path')
         if not wav_path_upload is None:
             if os.path.isfile(wav_path_upload) and wav_path_upload.endswith('wav'):
                 shutil.copy(wav_path_upload, 'raw')
-                return jsonify({'status':'OK','upload_file_path': wav_path_upload})
+                return jsonify({'status': 'OK', 'upload_file_path': wav_path_upload})
             else:
-                return jsonify({'FileError': '目标文件不存在/非wav文件'}) , 800
+                return jsonify({'FileError': '目标文件不存在/非wav文件'}), 800
         else:
-            return jsonify({'Error': '不是有效的参数'}) , 801
+            return jsonify({'Error': '不是有效的参数'}), 801
 
     except Exception as e:
         # 如果发生错误，返回异常信息
         print("服务端:Python发生错误：" + str(e))
-        return jsonify({'PythonError': str(e)}) , 800
-
-# 推理
-# 用法：POST请求
-# 请求参数参考README.md
-# 推理前请先加载模型
+        return jsonify({'PythonError': str(e)}), 800
 
 
+@app.route('/inference', methods=['post'])
+def inference():
+    try:
+        # 获取参数
+        clean_names_ls = request.args.get('clean_names_ls')
+        trans_num = request.args.get('trans_num')  # int
+        spk_list_ls = request.args.get('spk_list_ls')
+        slice_db_num = request.args.get('slice_db')  # int
+        output_file_type = request.args.get('output_file_type')
+        model_path = request.args.get('model_path')
+        json_path = request.args.get('json_path')
+        # 加载svc模型
+        if not model_path is None:
+            if not json_path is None:
+                if os.path.isfile(model_path) and model_path.endswith('pth'):  # 验证文件路径以及后缀名
+                    print("服务端:已导入模型文件 路径:" + model_path)
+                else:
+                    return jsonify({'PthFileError': '目标文件不存在/非pth文件'}), 800
 
-if __name__=='__main__':
+                if os.path.isfile(json_path) and json_path.endswith('json'):  # 验证文件路径以及后缀名
+                    print('服务端:已导入Config文件' + json_path)
+                else:
+                    return jsonify({'JsonFileError': '目标文件不存在/非json文件'}), 800
+            else:
+                return jsonify({'Error': '不是有效的参数'}), 801
+        else:
+            return jsonify({'Error': '不是有效的参数'}), 801
+
+        # 加载模型与创建文件夹
+        svc_model = Svc(model_path, json_path)  # 存入到G变量中
+        infer_tool.mkdir(["raw", "results"])
+
+        # 以下代码为读取说话人列表
+        with open(json_path, 'r') as config_json:
+            config_json_speaker = json.load(config_json)
+
+            for config_json_speaker_ls in config_json_speaker['spk']:
+                print('服务端:从Config.json文件中找到了以下说话人：' + config_json_speaker_ls)
+
+        print('服务端:模型加载已完成')
+        # 好了
+        # 这是一段很烂的判断（气笑）
+        if not clean_names_ls is None:
+            if not trans_num is None:
+                if not spk_list_ls is None:
+                    if not slice_db_num is None:
+                        # 判断非必填项是否填写,若未填写则填写默认值
+                        if output_file_type is None:
+                            output_file_type = 'wav'
+                        # 完事了，可以开始写正经代码了（
+                        # 为了省事所以再定义点变量 屎山实锤了
+                        clean_names = clean_names_ls.split(',')  # 按逗号分割多wav文件
+                        trans = (list(map(int, str(trans_num))))  # 调整半音数
+                        spk_list = spk_list_ls.split(',')  # 按逗号分割多说话人
+                        slice_db = float(slice_db_num)  # slice_db
+                        wav_format = str(output_file_type)  # 音频输出格式
+                        # 下面是照搬的推理代码
+                        infer_tool.fill_a_to_b(trans, clean_names)
+                        for clean_name, tran in zip(clean_names, trans):
+                            raw_audio_path = f"raw/{clean_name}"
+                            if "." not in raw_audio_path:
+                                raw_audio_path += ".wav"
+                            infer_tool.format_wav(raw_audio_path)
+                            wav_path = Path(raw_audio_path).with_suffix('.wav')
+                            chunks = slicer.cut(wav_path, db_thresh=slice_db)
+                            audio_data, audio_sr = slicer.chunks2audio(wav_path, chunks)
+                            print('SovitsBox正在推理')
+                            for spk in spk_list:
+                                audio = []
+                                for (slice_tag, data) in audio_data:
+                                    print(f'#=====分段起点, {round(len(data) / audio_sr, 3)}s======')
+                                    length = int(np.ceil(len(data) / audio_sr * svc_model.target_sample))
+                                    raw_path = io.BytesIO()
+                                    soundfile.write(raw_path, data, audio_sr, format="wav")
+                                    raw_path.seek(0)
+                                    if slice_tag:
+                                        print('SovitsBox:跳过空段')
+                                        _audio = np.zeros(length)
+                                    else:
+                                        out_audio, out_sr = svc_model.infer(spk, tran, raw_path)
+                                        _audio = out_audio.cpu().numpy()
+                                    audio.extend(list(_audio))
+
+                                res_path = f'./results/{clean_name}_{tran}key_{spk}.{wav_format}'
+                                soundfile.write(res_path, audio, svc_model.target_sample, format=wav_format)
+
+                                print('服务端:合成完毕，已保存到' + res_path)
+                                return jsonify({'status': 'OK', 'output_wav_path': res_path})
+                    else:
+                        return jsonify({'Error': '不是有效的参数'}), 801
+                else:
+                    return jsonify({'Error': '不是有效的参数'}), 801
+            else:
+                return jsonify({'Error': '不是有效的参数'}), 801
+        else:
+            return jsonify({'Error': '不是有效的参数'}), 801
+
+    except Exception as e:
+        # 如果发生错误，返回异常信息
+        print("服务端:Python发生错误：" + str(e))
+        return jsonify({'PythonError': str(e)}), 800
+
+
+# 至此全部完毕
+
+if __name__ == '__main__':
     app.run(host='127.0.0.1', port=65503)  # 默认走65503端口，4.0走65504端口
